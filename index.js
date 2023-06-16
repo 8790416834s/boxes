aef3a38c23456161780125f8cbddf56effd43ba7
sravankumar <cheripellysravan0411@gmail.com>
2023-06-16 08:36:13 +0000

    test cases passed but o/p not displayed

---
+++ index.js
@@ -0,0 +1,22 @@
+const Box = (props) => {
+  //  Write your code here.
+  const { text, box } = props;
+
+  <div className={box}>
+    <p>{text}</p>
+  </div>;
+};
+
+const element = (
+  //  Write your code here.
+  <div className="container">
+    <h1 className="heading">Boxes</h1>
+    <div className="box-container">
+      <Box text="small" box="green-box" />
+      <Box text="medium" box="blue-box" />
+      <Box text="large" box="pink-box" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
